a901526c28e6e815be10cc5b95c2569bfb0c2693
becored <becored0@gmail.com>
2022-07-18 19:09:11 +0900

    add patches

---
+++ patches/Sub_801109E_WarKeyDsBrk_Rewritten.c
@@ -0,0 +1,194 @@
+// @Description Sub_801109E_WarKeyDsBrk_Rewritten
+// @HookAddress 0x2DEDEC
+// @HookString P
+// Mode: Thumb
+// Made by beco
+
+enum WAR_STAT_NORMAL {
+	WALK,					//歩き
+	TURN,					//振り返り
+	STOP,					//止まり
+	JUMPB,					//ジャンプ前
+	JUMP,					//ジャンプ
+	SQUAT,					//しゃがみ
+	SSLIP,					//しゃがみすべり
+	SWALK,					//しゃがみ歩き
+	SJUMP,					//しゃがみジャンプ
+	STURN,					//しゃがみ振り返り
+	WKBRK,					//歩き止まり
+	DSBRK,					//最速走り止まり
+	SLIP,					//すべり始め（坂）
+	ROLLB,					//転がり前
+	ROLL,					//転がり
+	RJUMP,					//転がりジャンプ
+	RFLIP,					//転がり後の跳ね返り
+	DOKANU,					//土管上
+	DOKAND,					//土管下
+	MISS,					//ダメージ
+	MISS2,
+	HAJIK,					//敵はじく
+	ATTACK,					//アタック
+	DATTACK,				//最速アタック
+	AJUMP,					//アタックジャンプ
+	DAJUMP,					//最速アタックジャンプ
+	AFLIP,					//アタック後の跳ね返り
+	HIP,					//ヒップアタック（空中）
+	PHIP,					//パワーヒップアタック
+	HIP2,					//ヒップアタック（地面）
+	PHIP2,					//パワーヒップアタック（地面）
+	FUMI,					//踏んづけ跳ね
+	LIFT1,					//敵持ち上げ（軽い）
+	LIFT2,					//敵持ち上げ（重い）
+	THROW0,					//投げ準備
+	TAME,					//投げ溜め
+	THROW1,					//投げ（弱）
+	THROW2,					//投げ（強）
+	JTHROW,					//飛び投げ
+	TAMETURN,				//溜め反転
+	LDMOVE,					//はしご移動
+	LDSTOP,					//はしご止まり
+	LDDOWN,					//はしご飛び落り
+	NETMOVEY,				//金網移動Ｙ
+	NETMOVEX,				//金網移動Ｘ
+	NETSTOP,				//金網止まり
+	NETDOWN,				//金網飛び落り
+	HANERU,					//敵地震
+	AWATE,					//はしご・金網落ちる
+	GATEIN,					//扉入る
+	GATEOUT,				//扉出る
+	UPFACE,					//上向く
+	TSQUAT,					//積み木用しゃがみ
+	ASERI,					//あせり
+	WAIT0,					//待機ポーズ
+	WAIT1,					//待機ポーズ
+	BARBELL0,				//バーベル０
+	BARBELL1,				//バーベル１
+	ATWALK,					//オート歩き
+	WAIT2,					//縄跳び
+	CATCH,					//捕らえる
+	JCLEAR,					//空中でボス戦クリア
+	CLRWAIT,				//ボス戦クリア後
+	CLRTURN,				//クリア後ターン
+	CLRWALK,				//クリア後歩き
+	CLRKIME,				//決めポーズ
+	LSTWAIT,				//宝待ち（ラスボス）
+	LSTKIME,				//決めポーズ（ラスボス）
+	LSTAWATE,				//慌てる（ラスボス）
+
+	SLEEP,					//眠り
+	WAKEUP,					//目覚め
+	JUMPA,					//着地
+	DASH,					//最速走り
+	DJUMP,					//最速ジャンプ
+	JFLIP,					//跳ね返り
+	CLASH,					//激突
+	TAKARA,					//宝箱
+	RPOUSE,					//構え
+	GOWALK,					//ゴー
+};
+
+#define Sub_8001DA4_m4aSongNumStart ((int (*)(int)) 0x8001DA5)
+#define Sub_806DAC0_PanelYakuAllNum_TileEventId ((int (*)(unsigned short,unsigned short)) 0x806DAC1)
+
+#define CurrentEnemyData ((volatile struct EnemyDataStructure*) 0x3000104)
+#define EntityLeftOverStateList ((volatile unsigned char*) 0x3000564)
+#define WarioKey_React ((volatile unsigned int*) 0x82DEC70)
+#define WarioChng_React ((volatile unsigned int*) 0x82DECA0)
+#define WarioOthr_React ((volatile unsigned int*) 0x82DED90)
+#define WarioMove_React ((volatile unsigned int*) 0x82DECD0)
+
+#define Wario_ucReact (*(volatile unsigned char*) 0x3001898)
+#define Wario_usTimer (*(volatile unsigned short*) 0x30018A4)
+#define Wario_usMukiX (*(volatile unsigned short*) 0x30018A6)
+#define Wario_usMukiY (*(volatile unsigned short*) 0x30018A8)
+#define Wario_usPosX (*(volatile unsigned short*) 0x30018AA)
+#define Wario_usPosY (*(volatile unsigned short*) 0x30018AC)
+#define Wario_ucAnmTimer (*(volatile unsigned short*) 0x30018B6)
+#define Wario_ucAnmTimer_LOBYTE (*(volatile unsigned char*) 0x30018B6)
+#define Wario_ucAnmTimer_HIBYTE (*(volatile unsigned char*) 0x30018B7)
+#define Wario_sMvSpeedX (*(volatile signed short*) 0x30018AE)
+#define Wario_sMvSpeedY (*(volatile signed short*) 0x30018B0)
+#define WarJEff_LOBYTE (*(volatile unsigned char*) 0x3001950)
+#define WarioLift1 (*(volatile unsigned char*) 0x3001948)
+#define WarioLift2 (*(volatile unsigned char*) 0x3001949)
+#define cGmStartFlg (*(volatile unsigned char*) 0x3000C3F)
+#define usWarStopFlg (*(volatile unsigned char*) 0x30019F6)
+#define ucWarOffFlg (*(volatile unsigned char*) 0x30019F8)
+#define ucDisConFlg (*(volatile unsigned char*) 0x3000012)
+#define ucGateNum (*(volatile unsigned char*) 0x3000025)
+#define ucTimeUp (*(volatile unsigned char*) 0x3000047)
+#define PassageID (*(volatile unsigned char*) 0x3000002)
+#define InPassageLevelID (*(volatile unsigned char*) 0x3000003)
+#define usBgEvy (*(volatile unsigned char*) 0x3001870)
+
+#define byte_82F1428 ((volatile unsigned char*) 0x82F1428)
+
+#define Sub_8075F44_TmScInit ((void (*)()) 0x8075F45)
+#define Sub_801DE7C_EnemyInit ((void (*)()) 0x801DE7D)
+#define Sub_80711E8_GameInit_ColorSetWork_All ((void (*)()) 0x80711E9)
+#define Sub_806BF88_BgmChangeInit ((void (*)()) 0x806BF89)
+#define Sub_801D684_EnemyMain ((void (*)()) 0x801D685)
+#define Sub_8074808_WarioHeartMake ((void (*)()) 0x8074809)
+
+#define CurrentRoomId (*(volatile unsigned char*) 0x3000024)
+
+int Sub_801109E_WarKeyDsBrk_Rewritten() {
+    // Vanilla code
+    int v0;
+    int v1;
+    signed short v2;
+    short v3;
+    short v5;
+
+    v0 = (Sub_806DAC0_PanelYakuAllNum_TileEventId((Wario_usPosY + 1) & 0xFFFF, (Wario_usPosX - 30) & 0xFFFF) >> 16) & 0xFF;
+    v1 = (Sub_806DAC0_PanelYakuAllNum_TileEventId((Wario_usPosY + 1) & 0xFFFF, (Wario_usPosX + 30) & 0xFFFF) >> 16) & 0xFF;
+    if ( v0 != 11 && v1 != 11 )
+    {
+      v2 = 32;
+      if ( !Wario_usTimer )
+      {
+        ++Wario_usTimer;
+        Sub_8001DA4_m4aSongNumStart(9);
+      }
+    }
+    else
+    {
+      v2 = 1;
+      if ( !Wario_usTimer )
+      {
+        ++Wario_usTimer;
+        Sub_8001DA4_m4aSongNumStart(11);
+      }
+    }
+    if ( Wario_usMukiX & 0x10 )
+    {
+      if ( Wario_sMvSpeedX <= 48 )
+        v3 = Wario_sMvSpeedX - v2;
+      else
+        v3 = Wario_sMvSpeedX - 3;
+      Wario_sMvSpeedX = v3;
+      if ( v3 <= 0 )
+        return STOP;
+    }
+    else
+    {
+      if ( Wario_sMvSpeedX >= -48 )
+        v5 = Wario_sMvSpeedX + v2;
+      else
+        v5 = Wario_sMvSpeedX + 3;
+      Wario_sMvSpeedX = v5;
+      if ( v5 >= 0 )
+        return STOP;
+    }
+    if ( Wario_ucAnmTimer >= byte_82F1428[12 * Wario_ucAnmTimer_HIBYTE + 8] )
+    {
+      Wario_ucAnmTimer_LOBYTE = 0;
+      ++Wario_ucAnmTimer_HIBYTE;
+      if ( !byte_82F1428[12 * Wario_ucAnmTimer_HIBYTE + 8] )
+      {
+        Wario_ucAnmTimer_HIBYTE = 0;
+        WarJEff_LOBYTE = 5;
+      }
+    }
+    return 255;
+}
